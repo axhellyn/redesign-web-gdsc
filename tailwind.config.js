343c6e82401d7e0587506537ac57cea01ae54286
widya <widyadhana92@gmail.com>
2024-06-20 06:18:24 +0700

    ui: add upcoming events section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'primary':'#0052DC'
+        'primary':'#0052DC',
+        'primary-light':'#4385F5',
+        'secondary':'#F1F1F1',
+        'secondary-dark':'#B2B2B2',
+        'succes':'#2DA96A',
       },
       backgroundImage: {
         'gdsc-1': 'url("/assets/images/gdscPhoto1.png")',
