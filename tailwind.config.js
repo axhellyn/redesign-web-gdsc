4d6437dbeb532cb0ef43e04cd856659f561d2f97
widya <widyadhana92@gmail.com>
2024-06-20 04:15:17 +0700

    ui: add about us section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     extend: {
       colors:{
         'primary':'#0052DC'
+      },
+      backgroundImage: {
+        'gdsc-1': 'url("/assets/images/gdscPhoto1.png")',
+        'gdsc-2': 'url("/assets/images/gdscPhoto2.png")',
+        'gdsc-3': 'url("/assets/images/gdscPhoto3.png")',
+        'gdsc-4': 'url("/assets/images/gdscPhoto4.png")'
       }
     },
   },
